84ffaeb08cb492d4d8a274bc5d8a683f306248de
yatto17 <ysht17@gmail.com>
2021-11-10 14:39:32 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content, images: [])
+      params.require(:post).permit(:title, :content, images: []).merge(user_id: current_api_v1_user.id)
     end
 end
